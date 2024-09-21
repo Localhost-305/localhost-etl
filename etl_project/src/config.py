f85b5f39436d6802b5719b89c916cbb27e520464
Laroy Prado <laroyprado@gmail.com>
2024-09-21 18:01:05 -0300

    feature/LOC-2/inicia arquivo de configurações

---
+++ etl_project/src/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='etl_project\\env.env')
+
+DATABASE_URI = os.getenv('DATABASE_URI')
+DIRECTORY_PATH = os.getenv('DIRECTORY_PATH')
+LOG_DIRECTORY_PATH = os.getenv('LOG_DIRECTORY_PATH')
+COMPLETED_DIRECTORY_PATH = os.getenv('COMPLETED_DIRECTORY_PATH')
+ERROR_DIRECTORY_PATH = os.getenv('ERROR_DIRECTORY_PATH')
+
+os.makedirs(COMPLETED_DIRECTORY_PATH, exist_ok=True)
+os.makedirs(ERROR_DIRECTORY_PATH, exist_ok=True)
+os.makedirs(LOG_DIRECTORY_PATH, exist_ok=True)
