03f279ee1c8f385456c43327376435da69f4ce1a
Lukas <lkfernando12345@gmail.com>
2024-11-10 15:47:42 -0300

    feature/LOC-72/adicionando um print para ver se as variaveis de ambiente estão ok

--- etl_project/src/config.py
+++ etl_project/src/config.py
@@ -9,6 +9,8 @@ LOG_DIRECTORY_PATH = os.getenv('LOG_DIRECTORY_PATH')
 COMPLETED_DIRECTORY_PATH = os.getenv('COMPLETED_DIRECTORY_PATH')
 ERROR_DIRECTORY_PATH = os.getenv('ERROR_DIRECTORY_PATH')
 
+print('\n\n teste teste \n\n deu certo:\n ')
+print(DATABASE_URI + ' \n ' + DIRECTORY_PATH + ' \n ' + LOG_DIRECTORY_PATH + ' \n ' + COMPLETED_DIRECTORY_PATH + ' \n ' + ERROR_DIRECTORY_PATH)
 
 os.makedirs(COMPLETED_DIRECTORY_PATH, exist_ok=True)
 os.makedirs(ERROR_DIRECTORY_PATH, exist_ok=True)
