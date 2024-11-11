675369f8beaa4db0d22efb24745363b401905afb
Lukas <lkfernando12345@gmail.com>
2024-11-11 11:18:06 -0300

    feature/LOC-72/removendo os prints

--- etl_project/src/config.py
+++ etl_project/src/config.py
@@ -12,19 +12,3 @@ ERROR_DIRECTORY_PATH = os.getenv('ERROR_DIRECTORY_PATH')
 os.makedirs(COMPLETED_DIRECTORY_PATH, exist_ok=True)
 os.makedirs(ERROR_DIRECTORY_PATH, exist_ok=True)
 os.makedirs(LOG_DIRECTORY_PATH, exist_ok=True)
-
-
-
-
-
-def check_directory_creation(directory_path):
-    if os.path.isdir(directory_path):
-        print(f"\nDiretório '{directory_path}' já existe ou foi criado com sucesso.")
-    else:
-        print(f"\nFalha ao criar ou acessar o diretório '{directory_path}'.")
-
-check_directory_creation(COMPLETED_DIRECTORY_PATH)
-check_directory_creation(ERROR_DIRECTORY_PATH)
-check_directory_creation(LOG_DIRECTORY_PATH)
-check_directory_creation(DIRECTORY_PATH)
-print()
