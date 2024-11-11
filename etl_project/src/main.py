245c995a5d90cf85ecca5e9d2a379b2f163e85b8
Lukas <lkfernando12345@gmail.com>
2024-11-11 14:48:00 -0300

    feature/LOC-72/adicionando mais prints para entender o porque o script nao esta iniciando

--- etl_project/src/main.py
+++ etl_project/src/main.py
@@ -6,3 +6,5 @@ if __name__ == "__main__":
     remove_pycache(os.getcwd())
     print('iniciando o monitoramento - arquivo atual: main.py')
     start_monitoring()
+
+print('arquivo main.py foi chamado...')
